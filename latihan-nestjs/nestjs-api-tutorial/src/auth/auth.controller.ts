462fa39e4e328b62e1933c1f878d7c61e6768f1b
Dandi Rizky <dandirizkyy94@gmail.com>
2023-04-27 17:50:47 +0700

    belajar nestjs 27-04-2023

--- latihan-nestjs/nestjs-api-tutorial/src/auth/auth.controller.ts
+++ latihan-nestjs/nestjs-api-tutorial/src/auth/auth.controller.ts
@@ -1,20 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common/decorators';
-import { ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common/decorators';
+import { HttpStatus, ParseIntPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     // console.log({
-    //   dto: dto,
-    // })
-    return this.authService.signup(dto);
-  }
-
+      //   dto: dto,
+      // })
+      return this.authService.signup(dto);
+    }
+    
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
--- latihan-nestjs/nestjs-api-tutorial/src/auth/auth.service.ts
+++ latihan-nestjs/nestjs-api-tutorial/src/auth/auth.service.ts
@@ -3,10 +3,12 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) {}
   async signup(dto: AuthDto) {
     try {
       //generate the password hash
@@ -19,9 +21,7 @@ export class AuthService {
           hash,
         },
       });
-      delete user.hash;
-      //return the saved user
-      return user;
+      return this.signToken(user.id, user.email)
       return { msg: 'Hello i have signup' };
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
@@ -55,10 +55,24 @@ export class AuthService {
         throw new ForbiddenException(
           'Credentials incorrect',
         );
-      
-    // send back the user
-    delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email)
     return { msg: 'Hello i have signin' };
+  }
+
+  async signToken(userId: number, email: string):Promise<{access_token: string}> {
+    const payload = {
+      sub: userId,
+      email
+    }
+    const secret = this.config.get('JWT_SECRET')
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret
+    })
+
+    return {
+      access_token: token,
+    }
   }
 }
